5b358645682d4654857f189e6586a37b2d9ad8ed
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-11-03 13:40:43 +0900

    avoid clone use reference instead (#21)

--- src/crud.rs
+++ src/crud.rs
@@ -3,12 +3,13 @@ use diesel::{
     r2d2::{ConnectionManager, PooledConnection},
     PgConnection, QueryResult, RunQueryDsl,
 };
+use serenity::model::id::GuildId;
 
 use crate::models::{Command, NewCommand};
 
 pub fn add_command(
     conn: PooledConnection<ConnectionManager<PgConnection>>,
-    guild_id: String,
+    guild_id: &GuildId,
     command: String,
     response: String,
     created_by: String,
@@ -16,7 +17,7 @@ pub fn add_command(
     use crate::schema::commands;
 
     let new_command = NewCommand {
-        guild_id,
+        guild_id: guild_id.to_string(),
         command,
         response,
         created_by,
@@ -29,26 +30,27 @@ pub fn add_command(
 
 pub fn get_all_commands(
     conn: PooledConnection<ConnectionManager<PgConnection>>,
-    gid: String,
-    command_key: String,
+    gid: &GuildId,
+    command_key: &str,
 ) -> QueryResult<Vec<Command>> {
     use crate::schema::commands::dsl::*;
 
     commands
-        .filter(guild_id.eq(gid))
+        .filter(guild_id.eq(gid.to_string()))
         .filter(command.eq(&command_key))
         .load::<Command>(&conn)
 }
 
 pub fn command_delete(
     conn: PooledConnection<ConnectionManager<PgConnection>>,
-    gid: String,
-    command_key: String,
+    gid: &GuildId,
+    command_key: &String,
 ) -> QueryResult<usize> {
     use crate::schema::commands::dsl::*;
+    let gid_str = gid.to_string();
     diesel::delete(
         commands
-            .filter(guild_id.eq(gid))
+            .filter(guild_id.eq(gid_str))
             .filter(command.eq(command_key)),
     )
     .execute(&conn)
